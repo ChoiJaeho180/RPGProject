ea85f1af23a20205e1f0e452bd92b1b95e806b5f
JaeHoCCC <jaeho7423@gmail.com>
2021-08-26 21:05:45 +0900

    Ue4 프로젝트 추가

---
+++ Source/RPGProject/RPGProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RPGProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGPROJECT_API ARPGProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
