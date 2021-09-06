469b09fc34c14d15ed1def732e43683641f68705
JaeHoCCC <jaeho7423@gmail.com>
2021-09-06 20:12:24 +0900

    Rest 클래스 추가

---
+++ Source/RPGProject/Private/Common/REST/RPGCommonRestRequest.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Common/REST/RPGCommonRestRequest.h"
+
+RPGCommonRestRequest::RPGCommonRestRequest()
+{
+}
+
+RPGCommonRestRequest::~RPGCommonRestRequest()
+{
+}
---
+++ Source/RPGProject/Public/Common/RPGCommonGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "RPGCommonGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGPROJECT_API URPGCommonGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
